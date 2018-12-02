ce88e2ff4e190589afdafbcc3baab3ec805f1e6f
CZX <516713700@qq.com>
2018-12-02 13:54:12 +0800

    简单修改

--- src/main/java/cn/edu/bupt/ConverterApplication.java
+++ src/main/java/cn/edu/bupt/ConverterApplication.java
@@ -3,6 +3,12 @@ package cn.edu.bupt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @Description: 启动类
+ * @Author: CZX
+ * @CreateDate: 2018/12/2 13:52
+ * @Version: 1.0
+ */
 @SpringBootApplication
 public class ConverterApplication {
 	public static void main(String[] args) {
