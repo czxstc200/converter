40acaef36b72ec8744d7b6e614661ce17af631db
czx <czxstc200@bupt.edu.cn>
2018-12-02 17:53:34 +0800

    增加listener和event

---
+++ src/main/java/cn/edu/bupt/stream/adapter/RtspVideoAdapter.java
@@ -0,0 +1,10 @@
+package cn.edu.bupt.stream.adapter;
+
+/**
+ * @Description: RtspVideoAdapter
+ * @Author: czx
+ * @CreateDate: 2018-12-02 17:52
+ * @Version: 1.0
+ */
+public class RtspVideoAdapter {
+}
