51d343a9142ed7d81eaf319e83f9ee590c2aef9f
czx <czxstc200@bupt.edu.cn>
2019-06-05 14:43:38 +0800

    Revert "删除Netty的web模块"
    This reverts commit af67d821

---
+++ transport/src/main/java/cn/edu/bupt/annotation/RequestMethod.java
@@ -0,0 +1,11 @@
+package cn.edu.bupt.annotation;
+
+/**
+ * @Description: RequestMethod
+ * @Author: czx
+ * @CreateDate: 2019-05-30 22:44
+ * @Version: 1.0
+ */
+public enum RequestMethod {
+    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE
+}
